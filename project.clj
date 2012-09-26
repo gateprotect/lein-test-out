f3a18a417bc1518dcfaf6204d439edf4e133955b
deduktion <jan@duktion.de>
2012-09-26 18:45:51 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,2 @@
 (defproject lein-test-out "0.2.1-SNAPSHOT"
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
-                                  [org.clojure/tools.namespace "0.1.3"]]}})
+  :dependencies [[org.clojure/tools.namespace "0.1.3"]]}})
